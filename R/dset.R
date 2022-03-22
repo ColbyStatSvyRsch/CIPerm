eb9b09121fe1d2f870ff231f18a7e64bdc898a16
Jerzy Wieczorek <jawieczo@colby.edu>
2022-03-22 10:47:56 -0400

    Replace old dset code with dsetFast code, and remove dsetFast

--- R/dset.R
+++ R/dset.R
@@ -25,8 +25,79 @@
 #' @export
 
 
-# TODO: can we get rid of loop somehow? should be much faster...
-#   YES, see dsetFast.R instead!
+######################
+#### Old notes-to-self from when we used to have two versions,
+####   dset() using for-loops vs a vectorized dsetFast().
+####   As of 3/22/2022, dsetFast() became the current version of dset() below,
+####   and the old version was simply removed.
+####   Look on GitHub for package versions 0.1.0.9004 or earlier
+####   (March 21, 2022 or earlier) to see old dset() code.
+##
+## CHECK TIMINGS AND OUTPUTS OF dset() VS dsetFast():
+##
+## Skipping the for loop with dsetFast() gives a ~7x speedup over original dset()!
+# > x <- c(19, 22, 25, 26)
+# > y <- c(23, 33, 40)
+# > microbenchmark::microbenchmark(dset(x, y), dsetFast(x, y), times = 1000)
+# Unit: microseconds
+#           expr      min       lq     mean   median       uq       max neval
+#     dset(x, y) 6276.401 6790.501 7715.261 7210.501 7910.150 51722.501  1000
+# dsetFast(x, y)  901.101 1060.551 1215.499 1141.201 1238.651  4548.201  1000
+##
+## And it does give the same result!
+##
+# > demo <- dset(x, y)
+# > demo2 <- dsetFast(x, y)
+# > all.equal(demo, demo2)
+# [1] TRUE
+##
+## Let's check this on other datasets too,
+## and let's check Monte Carlo runs with set.seed()...
+##
+## What about the larger groups of 6+9 obs instead of 4+3 obs?
+# > wl1 <- c(1.72, 1.64, 1.74, 1.70, 1.82, 1.82, 1.90, 1.82, 2.08)
+# > wl2 <- c(1.78, 1.86, 1.96, 2.00, 2.00, 1.96)
+# > microbenchmark::microbenchmark(dset(wl1, wl2), dsetFast(wl1, wl2), times = 50)
+# Unit: milliseconds
+#               expr       min       lq       mean     median        uq       max neval
+#     dset(wl1, wl2) 1138.0176 1226.140 1278.69760 1278.08755 1327.2168 1438.2427    50
+# dsetFast(wl1, wl2)   43.3406   47.194   50.46918   49.85155   52.8262   77.2373    50
+##
+## OH WOW, this is 25x faster now.
+## Here, plain dset() takes ~1sec each time, while dsetFast() is ~0.05sec.
+## I imagine the diffs will only get more extreme for larger datasets.
+##
+## And they still give same results?
+# > demo <- dset(wl1, wl2)
+# > demo2 <- dsetFast(wl1, wl2)
+# > all.equal(demo, demo2)
+# [1] TRUE
+# > demo <- dset(wl1, wl2, returnData = TRUE)
+# > demo2 <- dsetFast(wl1, wl2, returnData = TRUE)
+# > all.equal(demo, demo2)
+# [1] TRUE
+## Yes they do!
+##
+## What about Monte Carlo runs?
+## They shouldn't match without set.seed(), and indeed they don't...
+# > demo <- dset(wl1, wl2, nmc = 5000)
+# > demo2 <- dsetFast(wl1, wl2, nmc = 5000)
+# > isTRUE(all.equal(demo, demo2))
+# [1] FALSE
+##
+## But WITH set.seed, they DO match, as they should!
+# > seed = 20220321
+# > set.seed(seed)
+# > demo <- dset(wl1, wl2, nmc = 5000)
+# > set.seed(seed)
+# > demo2 <- dsetFast(wl1, wl2, nmc = 5000)
+# > all.equal(demo, demo2)
+# [1] TRUE
+##
+## So old dset() can be replaced with new dsetFast()
+######################
+
+
 
 # TODO: in the future, consider switching to RcppAlgos::comboSample(),
 # which lets you sample *directly* from the set of all possible combinations
@@ -61,36 +132,29 @@ dset <- function(group1, group2, nmc = 10000, returnData = FALSE){
     num <- nmc
   }
 
-  dataframe <- data.frame(diffmean = rep(NA, num),
-                          sum1 = rep(NA, num),
-                          diffmedian = rep(NA, num),
-                          wilsum = rep(NA, num),
-                          k = rep(NA, num),
-                          wkd = rep(NA, num))
+  dcombn2 <- apply(dcombn, 2, function(x) setdiff(1:N, x))
+  group1_perm <- matrix(combined[dcombn], nrow = n)
+  group2_perm <- matrix(combined[dcombn2], nrow = m)
 
-  group1_perm <- matrix(NA, n, num)
-  group2_perm <- matrix(NA, m, num)
+  k <- colSums(matrix(dcombn %in% ((n+1):N), nrow=n))
+  diffmean <- colMeans(group1_perm) - colMeans(group2_perm)
+  sum1 <- colSums(group1_perm)
+  diffmedian <- matrixStats::colMedians(group1_perm) - matrixStats::colMedians(group2_perm)
 
-  for (ii in 1:num){
+  r <- rank(combined, ties.method = "first")
+  wilsum <- colSums(matrix(r[dcombn], nrow = n))
+  wkd = (diffmean[1] - diffmean) / (k * den)
 
-    g1ind = dcombn[,ii]
-    g2ind = (1:N)[-g1ind]
-
-    group1_perm[,ii] <- combined[g1ind]
-    group2_perm[,ii] <- combined[g2ind]
-
-    dataframe$diffmean[ii] = mean(combined[g1ind]) - mean(combined[g2ind])
-    dataframe$sum1[ii] = sum(combined[g1ind])
-    dataframe$diffmedian[ii] = stats::median(combined[g1ind]) - stats::median(combined[g2ind])
-
-    r <- rank(combined, ties.method = "first")
-    rsum <- sum(r[g1ind])
-    dataframe$wilsum[ii] = rsum
-    dataframe$k[ii] = sum(!(g1ind %in% 1:n))  ## TODO: change to sum(g1ind > n) if faster?
-    dataframe$wkd[ii] = (dataframe$diffmean[1] - dataframe$diffmean[ii]) / (dataframe$k[ii] * den)
-  }
+  dataframe <- data.frame(diffmean = diffmean,
+                          sum1 = sum1,
+                          diffmedian = diffmedian,
+                          wilsum = wilsum,
+                          k = k,
+                          wkd = wkd)
 
   if(returnData){
+    # Return the whole table formatted as in Nguyen (2009)
+    # for comparison with his results
     w.i <- sort(dataframe$wkd, decreasing = FALSE, na.last = FALSE)
     ID <- c(1:num)
     dataset <- t(rbind(group1_perm, group2_perm))
@@ -100,4 +164,5 @@ dset <- function(group1, group2, nmc = 10000, returnData = FALSE){
     # Just return the fewest columns needed for p-vals and CIs
     return(dataframe)
   }
+
 }
